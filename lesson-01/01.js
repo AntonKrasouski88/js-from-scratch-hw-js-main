8d00e686387dd179a8c7434442e6e03e6fec45c4
Anton Krasovski <antohkad21@gmail.com>
2026-01-21 20:01:27 +0300

    feat: add sum calculation for number s from 1 to 20

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,3 +2,12 @@
 // Результат присвойте переменной sum
 
 // your code
+const START_NUMBER = 1;
+const FINISH_NUMBER = 20;
+let num = START_NUMBER;
+let sum = 0;
+
+while (num <= FINISH_NUMBER) {
+  sum += num;
+  num += 1;
+}
